0b419ff3a90db3d99fb7e7145181e18d4891df5a
kevinjn03 <114575604+KevinJN03@users.noreply.github.com>
2023-08-19 18:52:56 +0100

    dd pagination to message route

--- server/routes/message.js
+++ server/routes/message.js
@@ -1,21 +1,47 @@
 const express = require("express");
 const router = express.Router();
-const Message = require("../model/Message")
-const User = require("../model/User")
-const Controller = require("../controller/messageController")
-router.get("/", Controller.message_get)
+const Message = require("../model/Message");
+const User = require("../model/User");
+const Controller = require("../controller/messageController");
+router.get("/", Controller.message_get);
 
-router.post("/create", Controller.message_create)
+router.post("/create", Controller.message_create);
 
+router.get("/query", async (req, res, next) => {
+  const page = parseInt(req.query.page);
+  const limit = parseInt(req.query.limit);
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
 
-// router.get("/one", async(req, res, next) => {
-    
-// const message = await Message.findOne({}).populate("beautifyDate");
-// res.send(message)
+  const [messages, count] = await Promise.all([
+    Message.find()
+      .populate({ path: "author", select: "first_name" })
+      .limit(limit)
+      .skip(startIndex),
+    Message.countDocuments().exec(),
+  ]);
 
+  const results = {};
+  results.count = Math.ceil(count / limit)
+  results.message = messages;
 
+  results.next = {
+    page: page + 1,
+    limit: limit,
+  };
 
+  if (startIndex > 0)
+    results.previous = {
+      page: page - 1,
+    };
 
+  if (endIndex < count)
+    results.next = {
+      page: page + 1,
+    };
+
+  return res.send({ page, limit, results });
+});
 
 // })
-module.exports = router
+module.exports = router;
